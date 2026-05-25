928dd30cbc1c46d1022004cf5d288900e0b298f1
Simo <mohamedaarab.cbr600rr@gmail.com>
2026-05-25 05:16:52 +0100

    fix axios baseURL for production

--- src/axios.js
+++ src/axios.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000",
   withCredentials: true,
 });
 
-// ✅ كل request يضيف التوكن
 instance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
