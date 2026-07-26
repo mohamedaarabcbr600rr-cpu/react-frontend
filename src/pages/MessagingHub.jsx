89958d49d5091e1f0f3c889abec60325480b0cf6
Simo <mohamedaarab.cbr600rr@gmail.com>
2026-07-26 20:01:28 +0100

    fix: correct case-sensitive import path for GroupsPanel (Windows vs Linux build)

--- src/pages/MessagingHub.jsx
+++ src/pages/MessagingHub.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import Messagerie from "./Messagerie";
-import GroupsPanel from "./Groups/GroupsPanel";
+import GroupsPanel from "./groups/GroupsPanel";
 import "./MessagingHub.css";
 
 const MessagingHub = ({ authUserId }) => {
